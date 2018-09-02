a418b2074042b2a52b016eed57685b9c2d6bafde
Safet Berberi <berberi.safet@gmail.com>
2018-09-01 21:30:44 -0400

    added basic layout and removed some unneeded files

--- src/App.js
+++ src/App.js
@@ -1,19 +1,25 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Navbar from './components/layout/Navbar';
+import {BrowserRouter as Router,Route,Switch} from 'react-router-dom';
+import Index from './components/layout/Index';
 import './App.css';
+import {Provider} from './context';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+    <Provider>
+      <Router>
+            <React.Fragment>
+                <Navbar />
+                <div className="container">
+                    <Switch>
+                        <Route exact="/" component={Index} />
+                    </Switch>
+                </div>
+            </React.Fragment>
+        </Router>
+    </Provider>
     );
   }
 }
